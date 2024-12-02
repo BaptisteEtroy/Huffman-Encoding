4d94f3de4e802cdd36188ddbcf77f56ebcda6b02
talineshawwaa <talineshawwa15@gmail.com>
2024-12-02 18:06:44 +0100

    added error handling

--- Huffman_C/main.cpp
+++ Huffman_C/main.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc != 4) {
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        cerr << "Error: Invalid arguments provided.\n"
+             << endl;
         cerr << "Usage: " << argv[0] << " [c|d] input_file output_file" << endl;
         return 1;
     }
@@ -13,26 +17,40 @@ int main(int argc, char* argv[]) {
     string inputFile = argv[2];
     string outputFile = argv[3];
 
-    if (mode == "c") {
-        Compression compressor;
-        if (!compressor.compress(inputFile, outputFile)) {
-            cerr << "Compression failed!" << endl;
-            return 1;
-        } else {
-            cout << "Compression completed successfully." << endl;
+    try
+    {
+        if (mode == "c")
+        {
+            Compression compressor;
+            if (!compressor.compress(inputFile, outputFile))
+            {
+                throw runtime_error("Compression failed!");
+            }
+            else
+            {
+                cout << "Compression completed successfully." << endl;
+            }
         }
-    } else if (mode == "d") {
-        Decompression decompressor;
-        if (!decompressor.decompress(inputFile, outputFile)) {
-            cerr << "Decompression failed!" << endl;
-            return 1;
-        } else {
-            cout << "Decompression completed successfully." << endl;
+        else if (mode == "d")
+        {
+            Decompression decompressor;
+            if (!decompressor.decompress(inputFile, outputFile))
+            {
+                throw runtime_error("Decompression failed!");
+            }
+            else
+            {
+                cout << "Decompression completed successfully." << endl;
+            }
         }
-    } else {
-        cerr << "Invalid mode. Use 'c' for compression or 'd' for decompression." << endl;
+        else
+        {
+            throw invalid_argument("Invalid mode. Use 'c' for compression or 'd' for decompression.");
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
-
-    return 0;
 }
